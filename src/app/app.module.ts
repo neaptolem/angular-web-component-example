1e4e585493d80cf27fd785b49993de7e0cba2345
Andrian Kobevko <admin@MacBook-Pro-admin.local>
2020-05-03 20:02:11 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [InfoBoxComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [InfoBoxComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(injector: Injector) {
+    const el = createCustomElement(InfoBoxComponent, { injector });
+    customElements.define('info-box', el);
+  }
+
+  ngDoBootstrap() { }
+}
