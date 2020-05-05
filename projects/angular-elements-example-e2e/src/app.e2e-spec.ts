c393dddbdab3737003b6894b0333d5cbec04ef8f
Andrian Kobevko <admin@MacBook-Pro-admin.local>
2020-05-05 18:55:24 +0300

    v1

--- projects/angular-elements-example-e2e/src/app.e2e-spec.ts
+++ projects/angular-elements-example-e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('web-component-example app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-elements-example!');
   });
 
   afterEach(async () => {
---
+++ projects/angular-elements-example/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { ElementZoneStrategyFactory } from 'elements-zone-strategy';
+import { AppComponent } from './app.component';
+import { MyGreeterComponent } from './my-greeter/my-greeter.component';
+
+@NgModule({
+  declarations: [
+    // AppComponent,
+    // We still need to declare the component
+    MyGreeterComponent
+  ],
+  imports: [BrowserModule],
+  providers: [],
+})
+export class AppModule {
+  constructor(injector: Injector) {
+    /**
+     * IMPORTANT
+     *
+     * There is a bug with zone.js, this fixes that.
+     */
+    const strategyFactory = new ElementZoneStrategyFactory(
+      MyGreeterComponent,
+      injector
+    );
+    const myGreeterContructor = createCustomElement(MyGreeterComponent, {
+      injector,
+      strategyFactory
+    });
+    customElements.define('my-greeter', myGreeterContructor);
+  }
+
+  ngDoBootstrap() {
+
+  }
+}
